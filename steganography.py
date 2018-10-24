6c2a4d91f6e6f84803bef5451fef063e4fe238ce
swapnil3597 <swapnil3597@gmail.com>
2018-10-24 20:05:06 +0530

    Modifications

--- steganography.py
+++ steganography.py
@@ -1,3 +1,13 @@
+'''
+This repository contains the Data Compression and Encryption ISE component-2 on Steganography by,
+
+    2015120023 - Akash Mahale
+    2015120025 - Swapnil Masurekar
+    2015120033 - Prathamesh Pai
+    2015120035 - Vedanta Pawar
+
+'''
+
 import os , sys
 import wave , struct 
 import binascii
